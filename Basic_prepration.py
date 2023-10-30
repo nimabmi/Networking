4fe92e192b12c8a7ecbfda112ba2b4e87f982ade
nimabmi <79307158+nimabmi@users.noreply.github.com>
2023-10-30 14:55:09 +0100

    exception testing

--- Basic_prepration.py
+++ Basic_prepration.py
@@ -1,1 +1,11 @@
-print("hello")
+def printExceptionsTree(ExceptionClass, level = 0):
+    if level > 1:
+        print("   |" * (level - 1), end="")
+    if level > 0:
+        print("   +---", end="")
+ 
+    print(ExceptionClass.__name__)
+    for subclass in ExceptionClass.__subclasses__():
+        printExceptionsTree(subclass, level+1)
+ 
+printExceptionsTree(BaseException)
